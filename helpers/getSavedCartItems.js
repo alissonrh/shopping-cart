94d6f45713290dc2c277076828e51dcd4a08e88b
alissonRH <alisson.honorato@live.com>
2022-04-27 11:37:59 -0300

    arrumei o lint

--- helpers/getSavedCartItems.js
+++ helpers/getSavedCartItems.js
@@ -1,7 +1,6 @@
 function getSavedCartItems(item) {
-  /* const itemsCart = document.querySelector('.cart__items'); */
-  // eslint-disable-next-line no-param-reassign
-  item.innerHTML = localStorage.getItem('cartItems');
+  const theElement = item;
+  theElement.innerHTML = localStorage.getItem('cartItems');
 }
 
 if (typeof module !== 'undefined') {
